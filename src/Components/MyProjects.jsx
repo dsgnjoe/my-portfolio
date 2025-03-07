2d2d0d86b7fb08e6ec57816322feaf98775049bc
theboyce <kwabenaadofo330@gmail.com>
2025-03-07 02:20:01 +0000

    hover states done

--- src/Components/MyProjects.jsx
+++ src/Components/MyProjects.jsx
@@ -160,40 +160,22 @@ const MyProjects = () => {
           </div>
 
           {/* fourth project */}
-          <div className="bg-[#C2EFFF] rounded-2xl border-4 border-[#EBFAFF]">
-            <div className="flex flex-col gap-2 p-6">
-              <p className="text-base text-[#164564]">Kriblink</p>
-              <p className="text-2xl font-semibold">
-                Simplifying Off-Campus Housing for Students (Wins, Struggles,
-                and Lessons)
-              </p>
-            </div>
-            <div className="px-6 pt-3 pb-6">
-              <img
-                src={thriftfinder}
-                alt="cover image"
-                className="rounded-lg"
-              />
-            </div>
-          </div>
-
-          {/* fifth project */}
           <div>
             <motion.div
               className="relative  rounded-2xl overflow-hidden cursor-pointer h-full"
               whileHover={{ scale: 1.0 }}
             >
               <motion.div
-                className="absolute inset-0 bg-[#C2D6FF]/80 border- border-[#EBF1FF] flex flex-col items-start justify-center text-white opacity-0 backdrop-blur-lg p-8 rounded-2xl"
+                className="absolute inset-0 bg-[#C2EFFF]/80 border-4 border-[#EFFAF6] flex flex-col items-start justify-center text-white opacity-0 backdrop-blur-lg p-8 rounded-2xl"
                 initial={{ opacity: 0 }}
                 whileHover={{ opacity: 1 }}
                 transition={{ duration: 0.3 }}
               >
                 <div className="flex flex-col gap-8 items-start">
                   <div className="flex flex-col gap-3">
-                    <p className="text-base text-[#253EA7]">ThriftFinder</p>
+                    <p className="text-base text-[#176448]">Betta UI</p>
                     <h3 className="text-2xl font-semibold text-black">
-                      Making Instagram Shop Discovery Easier With ThriftFinder
+                      A comprehensive mini design system for very small projects
                     </h3>
                   </div>
 
@@ -207,16 +189,16 @@ const MyProjects = () => {
                   </a>
                 </div>
               </motion.div>
-              <div className="bg-[#C2D6FF] flex flex-col gap-3 rounded-2xl border-4 border-[#EBF1FF]">
+              <div className="bg-[#C2EFFF] flex flex-col gap-3 rounded-2xl border-4 border-[#EBFAFF]">
                 <div className="flex flex-col gap-2 p-6">
-                  <p className="text-base text-[#162664]">ThriftFinder</p>
+                  <p className="text-base text-[#164564]">Betta UI</p>
                   <p className="text-2xl font-semibold">
-                    Making Instagram Shop Discovery Easier With ThriftFinder
+                    A comprehensive mini design system for very small projects
                   </p>
                 </div>
-                <div className="px-6 flex-1 flex justify-center">
+                <div className="px-6 flex-1 flex justify-center pb-6">
                   <img
-                    src={thriftfinder}
+                    src={kriblink}
                     alt="cover image"
                     className="rounded-lg"
                   />
