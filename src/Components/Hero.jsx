03c1ef8eb02b978fb6c5f48051d344bad3a4d97f
theboyce <kwabenaadofo330@gmail.com>
2025-03-13 21:01:56 +0000

    starting mobile responsive

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,15 +7,15 @@ const Hero = () => {
     <div className="">
       <Container>
         <div className="container px-5 lg:py-[120px]">
-          <h6 className="text-[16px] pb-5 lg:text-xl text-gray-500">
+          <h6 className="text-base pb-5 lg:text-xl text-gray-500">
             Hi, I’m Joseph.
           </h6>
-          <p className="text-[24px] lg:text-6xl font-bold pb-4">
+          <p className="text-[28px] md:text-6xl font-bold pb-4">
             A <span className="text-teal-700">Product</span> Designer,{" "}
             <span className="text-orange-700">Strategic</span> Thinker, and
             Creative Problem Solver
           </p>
-          <p className="text-xl max-w-6xl text-gray-600 pb-5">
+          <p className="text-base md:text-xl max-w-6xl text-gray-600 pb-5">
             With 3 years+ of product design experience, I turn complex problems
             into intuitive and engaging digital experiences that not only
             work—but wow. Every pixel and interaction is crafted to connect with
