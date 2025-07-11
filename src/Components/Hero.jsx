52e7290154607c8ba17646eb757c75081bd2c111
theboyce <kwabenaadofo330@gmail.com>
2025-07-11 11:39:53 +0000

    hero button fix

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,35 +5,45 @@ import Navbar from "../Components/Navbar";
 import heroBgPattern from "/images/hero-bg-pattern.png";
 import { RiMessage3Line } from "react-icons/ri";
 
-
 const Hero = () => {
-	return (
-		<div className="mb-8 md:mb-0">
-			<Container className="">
+  return (
+    <div className="mb-8 md:mb-0">
+      <Container className="">
         <Navbar />
-				<div className="lg:container text-center md:px-3 py-5 md:pb-[160px] pt-[160px] lg:pt-[200px] relative z-10">
-					<h1 className="text-[28px] md:text-5xl mb-2">
-						Hello, I’m Joseph. A{" "}
-						<span className="text-[#525866] font-fraunces">Product Designer</span>
-					</h1>
-					<h1 className="text-[28px] md:text-5xl mb-2">
-						focused on designing experiences that make
-					</h1>
-					<h1 className="text-[28px] md:text-5xl mb-2">
-						life <span className="text-[#525866] font-fraunces">easier</span> and more{" "}
-						<span className="text-[#525866] font-fraunces"> enjoyable</span>.
-					</h1>
-					<div className="text-center mt-8 mb-8">
-						<button className="px-5 py-3 mx-auto transition-all bg-[#C2540A] hover:bg-[#904d20] text-white rounded-xl flex items-center gap-2">
-						  <RiMessage3Line className="text-white" /> Get in Touch
-						</button>
-					</div>
-				</div>
+        <div className="lg:container text-center md:px-3 py-5 md:pb-[160px] pt-[160px] lg:pt-[200px] relative z-10">
+          <h1 className="text-[28px] md:text-5xl mb-2">
+            Hello, I’m Joseph. A{" "}
+            <span className="text-[#525866] font-fraunces">
+              Product Designer
+            </span>
+          </h1>
+          <h1 className="text-[28px] md:text-5xl mb-2">
+            focused on designing experiences that make
+          </h1>
+          <h1 className="text-[28px] md:text-5xl mb-2">
+            life <span className="text-[#525866] font-fraunces">easier</span>{" "}
+            and more{" "}
+            <span className="text-[#525866] font-fraunces"> enjoyable</span>.
+          </h1>
+          <div className="text-center mt-8 mb-8">
+            <a
+              href="mailto:kwabenaadofo330@gmail.com"
+              target="_blank"
+              className="px-5 py-3 mx-auto transition-all bg-[#C2540A] hover:bg-[#904d20] text-white rounded-xl flex items-center gap-2 w-fit"
+            >
+              <RiMessage3Line className="text-white" /> Get in Touch
+            </a>
+          </div>
+        </div>
 
-        <img src={heroBgPattern} alt="" className="absolute bottom-0 left-0 right-0 w-full z-0" />
-			</Container>
-		</div>
-	);
+        <img
+          src={heroBgPattern}
+          alt=""
+          className="absolute bottom-0 left-0 right-0 w-full z-0"
+        />
+      </Container>
+    </div>
+  );
 };
 
 export default Hero;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,25 +1,32 @@
 import React, { useState } from "react";
 import { MdArrowOutward } from "react-icons/md";
-import { FaBars, FaLinkedin, FaBehance, FaTimes, FaFile, FaFileAlt } from "react-icons/fa";
+import {
+  FaBars,
+  FaLinkedin,
+  FaBehance,
+  FaTimes,
+  FaFile,
+  FaFileAlt,
+} from "react-icons/fa";
 
 import logo from "/images/logo.svg";
 import Container from "./Container";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
- const [isScrolled, setIsScrolled] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
-  
+
   const onScroll = () => {
     if (window.scrollY > 80) {
       setIsScrolled(true);
     } else {
       setIsScrolled(false);
     }
-  }
+  };
 
   // Add event listener with cleanup
   React.useEffect(() => {
@@ -27,70 +34,32 @@ const Navbar = () => {
     return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
-
   return (
     <div>
-        <nav className={`bg-white px-4 py-4 w-full shadow rounded-2xl z-50 transition-all duration-300  fixed left-0 right-0  mx-auto max-w-[87%] md:max-w-[91%] xl:max-w-[716px] ${isScrolled ? "top-2 top-unset " : ""}`}>
-          <div className="container mx-auto flex justify-between items-center">
-            <a href="/" className="text-white text-lg md:text-2xl font-bold">
-              <img src={logo} alt="" />
-            </a>
-            <div className="block lg:hidden">
-              <button
-                onClick={toggleSidebar}
-                className="w-10 h-10 bg-black text-white flex justify-center items-center rounded-xl focus:outline-none"
-              >
-                {isOpen ? <FaTimes /> : <FaBars />}
-              </button>
-            </div>
-
-            <ul className="hidden lg:flex items-center space-x-10">
-              <li>
-                <a
-                  href="https://drive.google.com/file/d/1Uezx2LXyztz483zrjNpfOCBI0I81EhE-/view?usp=drive_link"
-                  target="_blank"
-                  className="text-black hover:text-gray-600"
-                >
-                  Resume
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.linkedin.com/in/josephadofo/"
-                  target="_blank"
-                  className="text-black hover:text-gray-600 flex items-center gap-2"
-                >
-                  {/* <FaLinkedin /> */}
-                  LinkedIn
-                </a>
-              </li>
-
-              <li>
-                <a
-                  href="mailto:kwabenaadofo330@gmail.com"
-                  target="_blank"
-                  className="text-white bg-black rounded-lg  px-3.5 py-2.5  text-[14px]"
-                >
-                  Get In Touch
-                </a>
-              </li>
-            </ul>
+      <nav
+        className={`bg-white px-4 py-4 w-full shadow rounded-2xl z-50 transition-all duration-300  fixed left-0 right-0  mx-auto max-w-[87%] md:max-w-[91%] xl:max-w-[716px] ${
+          isScrolled ? "top-2 top-unset " : ""
+        }`}
+      >
+        <div className="container mx-auto flex justify-between items-center">
+          <a href="/" className="text-white text-lg md:text-2xl font-bold">
+            <img src={logo} alt="" />
+          </a>
+          <div className="block lg:hidden">
+            <button
+              onClick={toggleSidebar}
+              className="w-10 h-10 bg-black text-white flex justify-center items-center rounded-xl focus:outline-none"
+            >
+              {isOpen ? <FaTimes /> : <FaBars />}
+            </button>
           </div>
-      {/* Mobile Nav */}
 
-
-      {isOpen && (
-        <div className="lg:hidden w-full bg-white z-50 flex flex-col transition-all ease-in-out">
-         
-          <ul
-            className="space-y-6 md:space-y-14
-           flex flex-col items-center justify-center h-full w-full"
-          >
+          <ul className="hidden lg:flex items-center space-x-10">
             <li>
               <a
-                href="https://www.linkedin.com/in/josephadofo/"
-                className="text-[#525866] text-lg md:text-2xl hover:text-gray-400"
-                onClick={toggleSidebar}
+                href="https://drive.google.com/file/d/1Uezx2LXyztz483zrjNpfOCBI0I81EhE-/view?usp=drive_link"
+                target="_blank"
+                className="text-black hover:text-gray-600"
               >
                 Resume
               </a>
@@ -98,14 +67,53 @@ const Navbar = () => {
             <li>
               <a
                 href="https://www.linkedin.com/in/josephadofo/"
-                className="text-[#525866] text-lg md:text-2xl hover:text-gray-400"
-                onClick={toggleSidebar}
+                target="_blank"
+                className="text-black hover:text-gray-600 flex items-center gap-2"
               >
+                {/* <FaLinkedin /> */}
                 LinkedIn
               </a>
             </li>
 
-            <li className="w-full">
+            <li>
+              <a
+                href="mailto:kwabenaadofo330@gmail.com"
+                target="_blank"
+                className="text-white bg-black rounded-lg  px-3.5 py-2.5  text-[14px]"
+              >
+                Get In Touch
+              </a>
+            </li>
+          </ul>
+        </div>
+        {/* Mobile Nav */}
+
+        {isOpen && (
+          <div className="lg:hidden w-full bg-white z-50 flex flex-col transition-all ease-in-out">
+            <ul
+              className="space-y-6 md:space-y-14
+           flex flex-col items-center justify-center h-full w-full"
+            >
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/josephadofo/"
+                  className="text-[#525866] text-lg md:text-2xl hover:text-gray-400"
+                  onClick={toggleSidebar}
+                >
+                  Resume
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/josephadofo/"
+                  className="text-[#525866] text-lg md:text-2xl hover:text-gray-400"
+                  onClick={toggleSidebar}
+                >
+                  LinkedIn
+                </a>
+              </li>
+
+              <li className="w-full">
                 <a
                   href="mailto:kwabenaadofo330@gmail.com"
                   target="_blank"
@@ -113,17 +121,14 @@ const Navbar = () => {
                 >
                   Get In Touch
                 </a>
-                <button
-                  className="text-white bg-black rounded-lg  px-3.5 py-3.5 w-full  text-[14px]"
-                >
+                <button className="text-white bg-black rounded-lg  px-3.5 py-3.5 w-full  text-[14px]">
                   Get In Touch
                 </button>
               </li>
-          </ul>
-        </div>
-      )}
-        </nav>
-
+            </ul>
+          </div>
+        )}
+      </nav>
     </div>
   );
 };
